350e44fba6147b4872236ea419a7fc3ec3c1989c
komdoroid <kom.commoc@gmail.com>
2026-03-23 23:00:50 +0900

    solving NumberofIslands

--- TwoSum/step1.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        l, r = 0, 0
-        while l<len(nums):
-            diff = target - nums[l]
-            if diff in nums[l+1:]:
-                r = l + nums.index(diff) + 1
-                return ([l, r])
